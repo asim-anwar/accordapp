96395ecb3fc73af2a512f1500f467841272ba9b5
asim-anwar <92793043+asim-anwar@users.noreply.github.com>
2022-04-20 05:18:45 +0600

    heroku deploy

--- accordapp/accordapp/settings.py
+++ accordapp/accordapp/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%k@^q-jo@qlugci%24jei_6qixp8$=e)852bp=71&m#kc%9o+h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["seikai-management.herokuapp.com/", '127.0.0.1']
 # ALLOWED_HOSTS = ['ad45-182-163-112-194.ngrok.io']
 
 
