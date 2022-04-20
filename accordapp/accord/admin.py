6ff200b2012498983edaf65ab87817c7a8c262f1
asim-anwar <92793043+asim-anwar@users.noreply.github.com>
2022-04-20 19:01:36 +0600

    folder name change

--- accordapp/accord/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Lobby,Topic,Post,User
-
-admin.site.register(Lobby)
-admin.site.register(Topic)
-admin.site.register(Post)
-admin.site.register(User)
