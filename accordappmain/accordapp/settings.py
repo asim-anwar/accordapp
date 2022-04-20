7c48a4aca58170eb5284fcd6490d9435308f2608
asim-anwar <92793043+asim-anwar@users.noreply.github.com>
2022-04-20 19:40:17 +0600

    change in settings apps

--- accordappmain/accordapp/settings.py
+++ accordappmain/accordapp/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accord.apps.AccordConfig',
     'corsheaders',
-    'rest_framework'
+    'rest_framework',
+    'accordapp'
     # 'api',
 ]
 
